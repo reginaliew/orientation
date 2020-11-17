cba9acb7dc05405a9e574b4801b5b2a24028a6dd
Mint <reginaliew9@gmail.com>
2020-11-17 14:00:21 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -115,7 +115,7 @@ $(document).ready(function(){
 
         if(rotateDegrees <= 60 && rotateDegrees >= 40) {
             $("#instruction").css("opacity", "1");
-            $(".back").css("transform", "rotate(25deg)");
+            $(".back").css("transform", "rotate(20deg)");
         } else if(rotateDegrees <= 95 && rotateDegrees >= 85){
             $("#instruction").css("opacity", "1");
             $(".back").css("transform", "rotate(0deg)");
